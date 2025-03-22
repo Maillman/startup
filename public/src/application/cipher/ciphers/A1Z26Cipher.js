ae94e1d42dbe9f85e01b832c2cbae643a9de691a
Melvin Whitaker <awsome7mailman@gmail.com>
2025-03-21 21:03:11 -0600

    Refactor Ciphers: Fully Implemented and Prettified A1Z26 Cipher. (Phase 1 Complete)

--- public/src/application/cipher/ciphers/A1Z26Cipher.js
+++ public/src/application/cipher/ciphers/A1Z26Cipher.js
@@ -1,3 +1,4 @@
+import { CryptState } from "../core/cryptState";
 import { iCipherFunction, CipherFunction } from "../core/iCipher";
 
 /**
@@ -5,26 +6,43 @@ import { iCipherFunction, CipherFunction } from "../core/iCipher";
  * @implements {iCipherFunction}
  */
 export class A1Z26Cipher extends CipherFunction {
-    static name = "A1Z26 Cipher";
+  static name = "A1Z26 Cipher";
 
-    static encryptFunction(c, index, text, delimiter) {
-        let next_index = index[0]+1;
-        // console.log("A1Z26", c, index, text[next_index], next_index);
-        let encipher = this.handleCipher(c, (this.alphabet.indexOf(c)+1).toString());
-        if(next_index < text.length && this.alphabet.indexOf(c) !== -1 && this.alphabet.indexOf(text[next_index].toLowerCase()) !== -1) {
-            encipher += delimiter;
-        }
-        return encipher;
-    }
+  static appliedFunction = (c, index, key) =>
+    this.encryptFunction(c, index, "", "");
 
-    static decryptFunction(c, delimiter) {
-        if(c==delimiter) {
-            return '';
-        }
-        let findCharacter = this.alphabet[parseInt(c)-1];
-        if (this.alphabet.indexOf(findCharacter) === -1) {
-            return c;
-        }
-        return findCharacter;
+  static encryptFunction(c, index, text, delimiter) {
+    let next_index = index[0] + 1;
+    // console.log("A1Z26", c, index, text[next_index], next_index);
+    let encipher = this.handleCipher(
+      c,
+      (this.alphabet.indexOf(c) + 1).toString()
+    );
+    if (
+      next_index < text.length &&
+      this.alphabet.indexOf(c) !== -1 &&
+      this.alphabet.indexOf(text[next_index].toLowerCase()) !== -1
+    ) {
+      encipher += delimiter;
     }
+    return encipher;
+  }
+
+  static decryptFunction(c, delimiter) {
+    if (c == delimiter) {
+      return "";
+    }
+    let findCharacter = this.alphabet[parseInt(c) - 1];
+    if (this.alphabet.indexOf(findCharacter) === -1) {
+      return c;
+    }
+    return findCharacter;
+  }
+
+  static applyFunction(text, delimiter, cryptState) {
+    this.appliedFunction = (c, index, key) =>
+      cryptState.equals(CryptState.Encrypted)
+        ? this.encryptFunction(c, index, text, delimiter)
+        : this.decryptFunction(c, delimiter);
+  }
 }
