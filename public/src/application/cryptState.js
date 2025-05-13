c87273a314e56c4bb82e2b3c0586ad7a3e21833a
Melvin Whitaker <awsome7mailman@gmail.com>
2025-05-13 10:01:34 -0600

    Refactor Ciphers: Complete! (Phase 3 Complete)

--- public/src/application/cryptState.js
+++
@@ -1,9 +0,0 @@
-export class CryptState {
-    static Unknown = new CryptState('Unknown');
-    static Encrypted = new CryptState('Encrypted');
-    static Decrypted = new CryptState('Decrypted');
-
-    constructor(name) {
-        this.name = name;
-    }
-}
